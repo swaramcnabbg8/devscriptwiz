8b771f114b99db3b18058170e3b10730f66a62e8
swaramcnabbg8 <swaramcnabbg8@gmail.com>
2024-03-31 02:43:15 +0800

    1

---
+++ devscriptwiz.js
@@ -0,0 +1,17 @@
+// user_authentication_system.js
+
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    authenticate(username, password) {
+        return this.username === username && this.password === password;
+    }
+}
+
+// Example usage
+const user = new User('Alice', '123456');
+console.log(user.authenticate('Alice', '123456')); // true
+console.log(user.authenticate('Bob', 'password')); // false
